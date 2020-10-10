6125451e618b8c9a590c14e23b2b352405640636
Elita <eliana.ormeno@gmail.com>
2020-10-10 12:20:18 -0300

    Last details

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,26 +12,39 @@ export class HomeComponent implements OnInit {
   addedProducts = [];
   isEmpty: boolean = true;
   counter: number = 0;
+  hasError: boolean = false;
+  showErrorMsg: string = 'An error ocurred, please try again later';
 
   constructor(public apiService: ApiService) { }
 
   ngOnInit() {
     this.apiService.getProducts()
-      .subscribe( res => this.products = res );
+      .subscribe(
+        res => this.products = res,
+        error => this.hasError = true
+      );
   }
 
   addToCart(id) {
     this.apiService.getProductById(id)
-      .subscribe( res => {
-        this.insertIntoCart(res);
-      });
+      .subscribe( 
+        res => this.insertIntoCart(res), 
+        error => {
+          this.hasError = true;
+          this.scrollToTop();
+        }
+      );
   }
 
   insertIntoCart(product) {
-    let {image, name, price } = product;
+    let { image, name, price } = product;
     this.addedProducts.push({image, name, price});
     this.counter++;
     this.isEmpty = false;
+    this.scrollToTop();
   }
-    
+
+  scrollToTop() {
+    document.getElementById('top').scrollIntoView({behavior: "smooth"});
+  }
 }
